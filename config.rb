00f5a9d55c7ea00ae96a485fc94adc83b7d1a1f6
Marion Couesnon <marion@craftsmen.io>
2015-10-09 18:52:20 +0200

    Fix the previous and next links below each article
    Hard code the blog prefix "the-unicorn-trail/"

--- config.rb
+++ config.rb
@@ -26,13 +26,13 @@ end
 helpers do
   def link_to_previous_article(text)
     if !current_article.previous_article.blank?
-      link_to text, current_article.previous_article.url
+      link_to text, "/the-unicorn-trail#{current_article.previous_article.url}"
     end
   end
 
   def link_to_next_article(text)
     if !current_article.next_article.blank?
-      link_to text, current_article.next_article.url
+      link_to text, "/the-unicorn-trail#{current_article.next_article.url}"
     end
   end
 end
