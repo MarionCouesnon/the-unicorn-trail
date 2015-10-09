4d9a759026114e95f1d8f42240349352e1aef956
Marion Couesnon <marion@craftsmen.io>
2015-10-09 17:12:59 +0200

    display the layout for the footer

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '2.2.2'
 
 gem 'bourbon';
+gem 'font-awesome-middleman'
 gem 'middleman';
 gem 'middleman-blog';
 gem 'middleman-deploy';
--- config.rb
+++ config.rb
@@ -20,3 +20,17 @@ end
 configure :development do
  activate :livereload
 end
+
+helpers do
+  def link_to_previous_article(text)
+    if !current_article.previous_article.blank?
+      link_to text, current_article.previous_article.url
+    end
+  end
+
+  def link_to_next_article(text)
+    if !current_article.next_article.blank?
+      link_to text, current_article.next_article.url
+    end
+  end
+end
